53cf4b6949fbf8124a79f940f07f83a54e8a8de6
H_T_0 <hayato.taka75@gmail.com>
2023-02-18 18:26:57 +1100

    load env vars from .env file

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from pathlib import Path
+import os
+import dotenv
+
 from prep.scraper import BuildingApprovalsScraper
+
+
+# Get root dir
+BASE_DIR = Path(__file__).resolve().parent
+
+# Load environment file
+dotenv_file = os.path.join(BASE_DIR, ".env")
+dotenv.load_dotenv(dotenv_file, override=True)
+
 
 if __name__ == "__main__":
 
