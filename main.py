fc10461a1677015d0f9dd65371348a0b0b80b010
H_T_0 <hayato.taka75@gmail.com>
2023-02-18 18:37:31 +1100

    main.py to create a db table for building approvals

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import os
 import dotenv
+import boto3
 
 from prep.scraper import BuildingApprovalsScraper
+import prep.model as model
 
 
 # Get root dir
@@ -23,3 +25,24 @@ if __name__ == "__main__":
 
     else:
         ba_scraper.execute()
+
+    # Access database
+    db = boto3.resource(
+        service_name="dynamodb",
+        endpoint_url=os.environ.get("DB_ENDPOINT_URL"),
+        region_name=os.environ.get("AWS_DEFAULT_REGION"),
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY")
+    )
+    db_tables = [table.table_name for table in db.tables.all()]
+
+    # Access tables
+    model_ba = model.BuildingApprovals(db)
+
+    # Delete existing tables
+    if model_ba.table_name in db_tables:
+        model_ba.delete_table()
+
+    # Create empty tables from model.py
+    model_ba.create_table()
+    model_ba.__repr__()
